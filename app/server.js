ac8bf6b3877251d0785172a9a8323f0f3a404acb
gerbilord <cois.alessandro@gmail.com>
2020-07-29 23:34:39 -0700

    Fix basic file structure

---
+++ app/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static("express"));
+const port = process.env.PORT || 5656;
+
+
+app.listen(port, () => {console.log(`http://localhost:${port}`);});
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname +'/index.html'));
+});
+
