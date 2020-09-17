731624cc1f108e7c2817305b0ad53a0a8848bb74
GerbiLord <cois.alessandro@gmail.com>
2020-09-16 22:46:44 -0700

    Add first button on click

--- src/frontend/home.jsx
+++ src/frontend/home.jsx
@@ -8,21 +8,36 @@ import Button from "./baseComponents/Button";
 class Home extends React.Component {
     constructor(props){
         super(props);
-        this.state = undefined;
+        this.state = {selection: "none"};
     }
 
-    render(){ return (
-        <div>
-            <div className="button_list">
-                <h1>Ale Tavern</h1>
-                <input type="text" placeholder="Game Id" />
-                <input type="text" placeholder="Name" />
-                <Button buttonText="Join" />
-                <h3>or</h3>
-                <Button buttonText="Create" />
-            </div>
-        </div>
-    );
+    render(){
+
+        switch(this.state.selection) {
+
+            case "none":
+                return (
+                    <div>
+                        <div className="button_list">
+                            <h1>Ale Tavern</h1>
+                            <input type="text" placeholder="Game Id" />
+                            <input type="text" placeholder="Name" />
+                            <Button buttonText="Join" />
+                            <h3>or</h3>
+                            <Button buttonText="Create" clickHandler={()=>{this.setState({selection: "create"}) } } />
+                        </div>
+                    </div>
+                );
+                break;
+
+            case "create":
+                return(<h1>"CREATE SCREEN"</h1>);
+                break;
+
+            default:
+                return(<h2>"sad"</h2>);
+        }
+
     }
 }
 
