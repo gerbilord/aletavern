2808e6a0eff87caec2fbc2e549f1315decdd200d
Wh1t3Sail0r <54153384+Wh1t3Sail0r@users.noreply.github.com>
2021-06-29 09:42:38 -0700

    Added game logic for tomfoag

---
+++ src/frontend/games/tomfoag/tomfoagView.js
@@ -0,0 +1,11 @@
+// GUI
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class TomfoagView extends React.Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
